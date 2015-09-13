d6cbaf2cd5667e7f642db0f19b5043e8c9c656b7
MR.Y <programming@yatsukouji.com>
2015-09-13 22:39:41 +0900

    バリデーション、def createとformとcustomerモデル

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -9,8 +9,13 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.new(customer_params)
-    @customer.save
-    redirect_to @customer
+    # @customer.save
+    # redirect_to @customer
+    if  @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
   end
 
   def edit
