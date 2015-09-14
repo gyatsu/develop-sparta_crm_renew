c19be25767b38c9e711ec74b8a81fe192bc6206b
MR.Y <programming@yatsukouji.com>
2015-09-14 15:25:51 +0900

    companyモデルとcustomerモデルアソシエーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ActiveRecord::Base
+
   validates :family_name,
     presence: true, length: { maximum: 20 }
   validates :given_name,
@@ -6,6 +7,8 @@ class Customer < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
     presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  belongs_to :company
 
   def full_name(keisho)
     full_name = family_name + given_name + keisho
